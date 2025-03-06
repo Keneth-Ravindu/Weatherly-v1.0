16439e9497f3edb7980da7cd91bf3e6d2cfc4792
Keneth Ravindu <129732198+Keneth-Ravindu@users.noreply.github.com>
2025-03-07 01:16:21 +0530

    Initial Commit

--- Weatherly/src/components/Weatherly.jsx
+++ Weatherly/src/components/Weatherly.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import './Weatherly.css'
 import search_icon from '../assets/search.png'
 import clear_icon from '../assets/clear.png'
@@ -10,6 +10,64 @@ import snow_icon from '../assets/snow.png'
 import wind_icon from '../assets/wind.png'
 
 const Weatherly = () => {
+
+    const inputRef = useRef()
+    
+    const [weatherData, setWeatherData] = useState(false);
+
+    const allIcons = {
+        "01d": clear_icon,
+        "01n": clear_icon,
+        "02d": cloud_icon,
+        "02n": cloud_icon,
+        "03d": cloud_icon,
+        "03n": cloud_icon,
+        "04d": drizzle_icon,
+        "04n": drizzle_icon,
+        "09d": rain_icon,
+        "09n": rain_icon,
+        "10d": rain_icon,
+        "10n": rain_icon,
+        "13d": snow_icon,
+        "13n": snow_icon,
+
+    }
+
+    const search = async (city) => {
+        if (city === "") {
+            alert("Please enter a valid city name.");
+            return;
+        }
+        try {
+            const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${import.meta.env.VITE_APP_ID}`;
+
+            const response = await fetch(url);
+            const data = await response.json();
+
+            if (!response.ok) {
+                alert(data.message);
+                return;
+            }
+
+            console.log(data);
+            const icon = allIcons[data.weather[0].icon] || clear_icon;
+            setWeatherData({
+                humidity: data.main.humidity,
+                windSpeed: data.wind.speed,
+                temperature: Math.floor(data.main.temp),
+                location: data.name,
+                icon: icon
+            })
+        } catch (error) {
+            setWeatherData(false);
+            console.error("Error while fetching weather data.")
+        }
+    }
+
+    useEffect(() => {
+        search("Manila");
+    }, [])
+
     return (
       
         <div className="weather">
@@ -18,33 +76,34 @@ const Weatherly = () => {
             </div>
         
         <div className="weather__search-bar">
-            <input type="text" placeholder="Enter a City Name" />
-                <div className="search_btn">
+            <input ref={inputRef} type="text" placeholder="Enter a City Name" />
+                <div className="search_btn" onClick={()=>search(inputRef.current.value)}>
                     <img src={search_icon} alt=""/>
             </div>
         </div>
-
-        
-            <img src={rain_icon} alt="" className = "weather__icon" />
-            <p className="temperature">16°C</p>
-            <p className="location">London</p>
+            {weatherData ? <>
+                            <img src={weatherData.icon} alt="" className = "weather__icon" />
+            <p className="temperature">{weatherData.temperature}°C</p>
+            <p className="location">{weatherData.location}</p>
 
             <div className="weather__data">
                 <div className="col">
                     <img src={humidity_icon} alt="" />
                     <div>
-                        <p>91%</p>
+                        <p>{weatherData.humidity} %</p>
                         <span>Humidity</span>
                     </div>
                 </div>
                 <div className="col">
                     <img src={wind_icon} alt="" />
                     <div>
-                        <p>3.6 Km/h</p>
+                        <p>{weatherData.windSpeed} Km/h</p>
                         <span>Wind Speed</span>
                     </div>
                 </div>              
             </div>  
+            </>:<></>}
+    
         
           
           
